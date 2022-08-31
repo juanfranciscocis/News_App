6abb590a1b227d3aa39e5adcdf9ab1cbfbcec985
Juan Cisneros <juanfrancistm2011@icloud.com>
2022-08-31 18:57:13 -0500

    BOTTOM NAVIGATION BAR

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,17 +6,52 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.blue,
-          ),
-        ],
-      )
+      body: _Pages(),
+      bottomNavigationBar: _BottomNavigation(),
+    );
+  }
+}
+
+class _BottomNavigation extends StatelessWidget {
+  const _BottomNavigation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'For You',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          label: 'Headlines',
+        ),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  const _Pages({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+      ],
     );
   }
 }
